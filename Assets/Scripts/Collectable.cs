d447c7a0c7b97a15ee712af0d21c92ca1ca5439b
DESKTOP-MTDE0GC\marxpark <ajvs0003@red.ujaen.es>
2020-12-24 12:23:11 +0100

    [24-12-2020] Primer commit antes de las plataformas en FTGD

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    private bool isCoin;
+    private bool isHealth;
+    private bool isAmmo;
+    private bool isInventoryItem;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Allows the controller to move each layer based on the parallaxAmount!*/
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [Range(-1f, 1f)]
+    public float parallaxAmount; //The amount of parallax! 1 simulates being close to the camera, -1 simulates being very far from the camera!
+    [System.NonSerialized] public Vector3 newPosition;
+    [SerializeField] public float scalar = 40f;
+    [SerializeField] bool horizontalOnly = true;
+    public void MoveLayer(float positionChangeX, float positionChangeY)
+    {
+        newPosition = transform.localPosition;
+        if (horizontalOnly)
+        {
+            newPosition.x -= positionChangeX * (-parallaxAmount * scalar) * (Time.deltaTime);
+        }
+        else
+        {
+            newPosition.x -= positionChangeX * (-parallaxAmount * scalar) * (Time.deltaTime);
+            newPosition.y -= positionChangeY * (-parallaxAmount * scalar) * (Time.deltaTime);
+        }
+
+
+        transform.localPosition = newPosition;
+    }
+}
